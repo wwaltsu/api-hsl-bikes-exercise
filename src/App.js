050a17144fec7f198ab33be816d3572b45e42110
wwaltsu <walter.ruoppa@metropolia.fi>
2020-09-04 22:29:56 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([])
+
+  useEffect( () => {
+    fetch("https://api.citybik.es/v2/networks/citybikes-helsinki")
+    .then(res => res.json())
+    .then(res => setData(res.network.stations) )
+  }, [])
+  
+  const listBikes = () => data.map(e => 
+    <tr key={e.name}>
+      <td>{e.name}</td>
+      <td>{e.free_bikes}</td>
+      <td>{e.empty_slots}</td>
+    </tr>
+    )
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Aseman nimi</th>
+              <th>Pyöriä saatavilla</th>
+              <th>Paikkoja vapaana</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listBikes()}
+          </tbody>
+        </table>
+      </div>
+  )
 }
 
 export default App;
