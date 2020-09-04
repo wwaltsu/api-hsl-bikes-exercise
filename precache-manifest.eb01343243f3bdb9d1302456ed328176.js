829d00cdf6fa7a91c1096ab6b749e1b595da647f
wwaltsu <walter.ruoppa@metropolia.fi>
2020-09-04 22:26:14 +0300

    Updates

---
+++ precache-manifest.eb01343243f3bdb9d1302456ed328176.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd8730ef81b245cc2a53d81f61fe8a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e701800ff04374c53711",
+    "url": "/static/js/2.3732416c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3732416c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e6d3beaf9973a421d6",
+    "url": "/static/js/main.6a3bb8c6.chunk.js"
+  },
+  {
+    "revision": "924368d14c8850be2f9e",
+    "url": "/static/js/runtime-main.b19b05f2.js"
+  }
+]);
