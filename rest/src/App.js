f36e37210ba345dc6eee8ed47679f1bb11daad6e
waltsuu <walter.ruoppa@metropolia.fi>
2020-09-11 21:08:00 +0300

    abua

--- rest/src/App.js
+++ rest/src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Map, TileLayer,Marker,Popup  } from 'react-leaflet';
+import './index.css';
+const DEFAULT_LONGITUDE = 24.825037;
+const DEFAULT_LATITUDE = 60.178432;
 const App = () => {
+  const [bikes, setBikes] = useState([])
+ 
+  useEffect( () => {
+    fetch("https://api.citybik.es/v2/networks/citybikes-helsinki")
+    .then(res => res.json())
+    .then(res => setBikes(res.network.stations) )
+  }, [])
   return (
-    <div>HHHHHHHHHHHHHHAAAAAAAAAAAAAAAAAAA</div>
+    <Map center={DEFAULT_LATITUDE, DEFAULT_LONGITUDE}>
+      <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; < a href=http://osm.org/copyright>
+      OpenStreetMap</a> contributors"/>
+    <TileLayer/>
+    </Map>
   )
 }
 
