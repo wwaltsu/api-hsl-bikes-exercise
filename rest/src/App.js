fec10e0cffd10bfdcbb326a6d31dd754a0d32003
waltsuu <walter.ruoppa@metropolia.fi>
2020-09-12 20:26:58 +0300

    mikael odottaa jo

--- rest/src/App.js
+++ rest/src/App.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Map, TileLayer,Marker,Popup  } from 'react-leaflet';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProgressBar from 'react-bootstrap/ProgressBar'
 const DEFAULT_LONGITUDE = 24.826430
 const DEFAULT_LATITUDE = 60.179690
 const position = [DEFAULT_LATITUDE, DEFAULT_LONGITUDE]
-
-
+/*
+<p>Pyöriä vapaana asemalla tällä hetkellä {""}  
+          {station.free_bikes} / 
+          {station.empty_slots + station.free_bikes} </p>
+          <ProgressBar animated now ={station.free_bikes /(station.empty_slots + station.free_bikes)} />
+*/
 const App = () => {
   const [bikes, setBikes] = useState([])
   useEffect( () => {
@@ -14,19 +20,17 @@ const App = () => {
     .then(res => setBikes(res.network.stations) )
   }, [])
 
-  const listBikes = () => bikes.map(e => 
-    <div key={e.name}>
-      <p>{e.name}</p>
-    </div>
-    )
+ 
 
-  const bikes_stations = () => bikes.map(station => 
-    <Marker position = {[station.latitude, station.longitude]}>
+  const bikes_stations = () => bikes.map(station =>
+  <Marker position = {[station.latitude, station.longitude]}>
        <Popup>
          <h3>{station.name}</h3>
-        <p>Pyöriä vapaana asemalla tällä hetkellä {""}  
+          Pyöriä vapaana asemalla tällä hetkellä {""}  
           {station.free_bikes} / 
-          {station.empty_slots + station.free_bikes} </p>
+          {station.empty_slots + station.free_bikes}
+          <ProgressBar variant="success" animated now = 
+          {station.free_bikes/(station.free_bikes + station.empty_slots)*100} />
       </Popup>
     </Marker>
   )
