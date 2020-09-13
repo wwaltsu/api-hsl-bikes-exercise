501e8335b9a95b739ed6b25626b1f3bc6b07b9d3
Walter Ruoppa <walter.ruoppa@metropolia.fi>
2020-09-13 17:52:30 +0300

    done

--- rest/src/App.js
+++ rest/src/App.js
@@ -1,53 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { Map, TileLayer,Marker,Popup  } from 'react-leaflet';
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch, Route, Link
+} from 'react-router-dom'
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ProgressBar from 'react-bootstrap/ProgressBar'
-const DEFAULT_LONGITUDE = 24.826430
-const DEFAULT_LATITUDE = 60.179690
-const position = [DEFAULT_LATITUDE, DEFAULT_LONGITUDE]
-//{station.free_bikes/(station.free_bikes + station.empty_slots)*100}
-/*
-<p>Pyöriä vapaana asemalla tällä hetkellä {""}  
-          {station.free_bikes} / 
-          {station.empty_slots + station.free_bikes} </p>
-          <ProgressBar animated now ={station.free_bikes /(station.empty_slots + station.free_bikes)} />
-*/
-const App = () => {
-  const [bikes, setBikes] = useState([])
-  useEffect( () => {
-    fetch("https://api.citybik.es/v2/networks/citybikes-helsinki")
-    .then(res => res.json())
-    .then(res => setBikes(res.network.stations) )
-  }, [])
+import Map from './Map'
+import Pyorat from './Pyorat'
 
-  const bikes_stations = () => bikes.map(station => {
-    const percent = station.free_bikes/(station.free_bikes + station.empty_slots)*100
-      return (
-        <Marker position = {[station.latitude, station.longitude]}>
-          <Popup>
-            <h4>{station.name}</h4>
-              Pyöriä vapaana asemalla tällä hetkellä {""}
-              {station.free_bikes} / 
-              {station.empty_slots + station.free_bikes}
-              <ProgressBar variant={ percent < 30 ? "danger":  percent < 50 ? "warning": "success" } 
-              animated now = {percent} />
-          </Popup>
-        </Marker>
-      )
+
+const App = () => {
+    
+    const padding = {
+        padding: 5
     }
-  )
-  return (
-    <div>
-    <Map center={position} zoom={13}>
-      <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="&copy; < a href=http://osm.org/copyright>
-      OpenStreetMap</a> contributors"/>
-      {bikes_stations()}
-    </Map>
-    </div>
-  )
+
+    return (
+        <div className="container">
+            <Router>
+                <div>
+                <Link className="spaces" to="/">Koti</Link>
+                    <Link className="spaces" to="/Map">Map</Link>
+                </div>
+                <Switch>
+                    <Route path="/Map">
+                        <Map />
+                    </Route>
+                    <Route path="/">
+                        <Pyorat />
+                    </Route>
+                </Switch>
+
+            </Router>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ rest/src/Pyorat.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+
+
+const Bikes = () => {
+  const [data, setData] = useState([])
+
+  useEffect( () => {
+    fetch("https://api.citybik.es/v2/networks/citybikes-helsinki")
+    .then(res => res.json())
+    .then(res => setData(res.network.stations) )
+  }, []) 
+  
+  const listBikes = () => data.map(station => 
+    <tr key={station.name}>
+      <td>{station.name}</td>
+      <td>{station.free_bikes}</td>
+      <td>{station.empty_slots}</td>
+    </tr>
+    )
+  
+
+  return (
+    <div className="App">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th>Aseman nimi</th>
+              <th>Pyöriä vapaana</th>
+              <th>Telakoita vapaana</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listBikes()}
+          </tbody>
+        </table>
+      </div>
+  )
+}
+
+export default Bikes;
